fa4ddba7d5174afa089c148bb73a5c030a82f2d4
Perry <chenzhipeng901105@gmail.com>
2015-12-04 17:40:38 +0800

    finish chapter 7

--- Chapter7_UIStackView&Autolayout/VacationSpots/VacationSpots/Views/VacationSpotCell.swift
+++ Chapter7_UIStackView&Autolayout/VacationSpots/VacationSpots/Views/VacationSpotCell.swift
@@ -30,7 +30,21 @@ class VacationSpotCell: UITableViewCell {
 
   override func awakeFromNib() {
     super.awakeFromNib()
-
+    // 1
+    let layoutGuide = UILayoutGuide()
+    contentView.addLayoutGuide(layoutGuide)
+    
+    // 2
+    let topConstraint = layoutGuide.topAnchor.constraintEqualToAnchor(nameLabel.topAnchor)
+    
+    // 3
+    let bottomConstraint = layoutGuide.bottomAnchor.constraintEqualToAnchor(locationNameLabel.bottomAnchor)
+    
+    // 4
+    let centeringConstraint = layoutGuide.centerYAnchor.constraintEqualToAnchor(contentView.centerYAnchor)
+    
+    // 5
+    NSLayoutConstraint.activateConstraints([topConstraint, bottomConstraint, centeringConstraint])
     // TODO: Add layoutGuide code here to center the name and locationName labels vertically
   }
 }
